c11761c911eb8897445e5cdaf789f631f8d663c6
Thomas <tom@ioffer.com>
2013-09-09 14:08:56 -0700

    essentially abusing github's api

--- lib/twitter/configuration.rb
+++ lib/twitter/configuration.rb
@@ -1,22 +1,22 @@
 require 'twitter/base'
 
-module Twitter
-  class Configuration < Twitter::Base
-    attr_reader :characters_reserved_per_media, :max_media_per_upload,
-      :non_username_paths, :photo_size_limit, :short_url_length, :short_url_length_https
-    alias short_uri_length short_url_length
-    alias short_uri_length_https short_url_length_https
+# module Twitter
+#   class Configuration < Twitter::Base
+#     attr_reader :characters_reserved_per_media, :max_media_per_upload,
+#       :non_username_paths, :photo_size_limit, :short_url_length, :short_url_length_https
+#     alias short_uri_length short_url_length
+#     alias short_uri_length_https short_url_length_https
 
-    # Returns an array of photo sizes
-    #
-    # @return [Array<Twitter::Size>]
-    def photo_sizes
-      memoize(:photo_sizes) do
-        Array(@attrs[:photo_sizes]).each_with_object({}) do |(key, value), object|
-          object[key] = Twitter::Size.new(value)
-        end
-      end
-    end
+#     # Returns an array of photo sizes
+#     #
+#     # @return [Array<Twitter::Size>]
+#     def photo_sizes
+#       memoize(:photo_sizes) do
+#         Array(@attrs[:photo_sizes]).each_with_object({}) do |(key, value), object|
+#           object[key] = Twitter::Size.new(value)
+#         end
+#       end
+#     end
 
-  end
-end
+#   end
+# end
